cdfe2911904c44a6b8113342f6cbc2c304fac43f
Amusan Olabisi <138717993+Amusanolabisi@users.noreply.github.com>
2026-06-23 23:00:12 +0100

    Delete SQL/1a.sql

--- SQL/1a.sql
+++
@@ -1,6 +0,0 @@
--- Retrieve the first_name, last_name, and email of every customer who lives in Lagos.
---Sort results alphabetically by last_name, then by first_name.
-SELECT first_name, last_name, email
-FROM customers
-WHERE city = 'Lagos'
-ORDER BY last_name ASC, first_name ASC;
